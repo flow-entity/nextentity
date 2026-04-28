3b0814849a16132a0f499da45c58501cb4239c0c
HuangChengwei <huang.chengwei@icloud.com>
2026-04-28 13:18:57 +0800

    refactor: LazyValueMap 改用组合模式并移除 spring 模块双重事务包装
    - LazyValueMap: 从继承 NullableConcurrentMap 改为组合 ConcurrentHashMap，
      仅暴露 get/put/containsKey 必要接口，target 类型改为 Map<Method, Object>
    - EntityFactoryBuilder: 移除 wrapWithTransaction 调用及方法，
      因 AbstractRepository 已通过 @Transactional 注解管理事务

--- nextentity-core/src/main/java/io/github/nextentity/core/reflect/LazyValueMap.java
+++ nextentity-core/src/main/java/io/github/nextentity/core/reflect/LazyValueMap.java
@@ -1,27 +1,45 @@
 package io.github.nextentity.core.reflect;
 
-import io.github.nextentity.core.util.NullableConcurrentMap;
-
 import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /// 代理对象的方法值映射，提供线程安全的方法到值的映射，
 /// 并内置 {@link LazyValue} 延迟加载解析。
 ///
 /// @author HuangChengwei
 /// @since 2.2.2
-public class LazyValueMap extends NullableConcurrentMap<Method, Object> {
+public class LazyValueMap {
+
+    private final Map<Method, Object> target = new ConcurrentHashMap<>();
 
     /// 加载方法对应的值，如果值是 {@link LazyValue} 则触发加载并替换。
-    ///
-    /// 使用 {@link #compute} 保证原子性，同一方法仅有一个线程执行加载。
     ///
     /// @param method 方法
     /// @return 加载后的值
     public Object get(Method method) {
-        Object result = super.get(method);
+        Object result = target.get(method);
         if (result instanceof LazyValue lazyValue) {
             return lazyValue.get();
         }
         return result;
+    }
+
+    public void put(Method getter, Object value) {
+        target.put(getter, value);
+    }
+
+    public boolean containsKey(Method method) {
+        return target.containsKey(method);
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        return o instanceof LazyValueMap that && target.equals(that.target);
+    }
+
+    @Override
+    public int hashCode() {
+        return target.hashCode();
     }
 }
